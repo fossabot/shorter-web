ba951c97d83d934529ea7c1d5b99c32d1abe6491
Max Cheng <ofcoursemax@gmail.com>
2024-09-21 22:56:25 -0700

    feat: add middleware to protect route

---
+++ src/middleware.ts
@@ -0,0 +1,70 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+type AuthStatusResponse = {
+  isValid: boolean;
+  success: boolean;
+  error?: string;
+};
+
+async function checkAuthStatus(request: NextRequest) {
+  const sessionCookie = request.cookies.get("url_shortener_gh_session");
+  // console.log("checkAuthStatus Session cookie:", sessionCookie);
+  if (!sessionCookie) {
+    console.log("checkAuthStatus Session cookie not found");
+    return false;
+  }
+
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/auth/validate`,
+      {
+        headers: {
+          Cookie: `url_shortener_gh_session=${sessionCookie.value}`,
+        },
+      }
+    );
+    // console.log("checkAuthStatus Response:", response);
+    const data: AuthStatusResponse = await response.json();
+    // console.log("checkAuthStatus Data:", data);
+
+    if (!data.success && !data.isValid) {
+      console.log("checkAuthStatus Session cookie deleted");
+    }
+    return data.success && data.isValid;
+  } catch (error) {
+    console.error("Error validating session:", error);
+    return false;
+  }
+}
+
+export async function middleware(request: NextRequest) {
+  const isLoggedIn = await checkAuthStatus(request);
+
+  console.log("Is logged in:", isLoggedIn);
+
+  // List of public routes that don't require authentication
+  const publicRoutes = ["/login, /auth/callback"];
+
+  // Check if the requested path is a public route
+  const isPublicRoute = publicRoutes.some((route) =>
+    request.nextUrl.pathname.startsWith(route)
+  );
+
+  if (!isLoggedIn && !isPublicRoute) {
+    // Redirect to login if not authenticated and trying to access a protected route
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  if (isLoggedIn && isPublicRoute) {
+    // Redirect to dashboard if already authenticated and trying to access a public route
+    return NextResponse.redirect(new URL("/dashboard", request.url));
+  }
+
+  // Continue with the request if everything is fine
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/dashboard/:path*"],
+};
